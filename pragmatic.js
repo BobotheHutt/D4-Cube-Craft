07e3bb855a366efc6b122c32b4276770a0552599
BobotheHutt <jumprun4112@gmail.com>
2026-06-05 19:01:14 -0700

    Create pragmatic.js

---
+++ pragmatic.js
@@ -0,0 +1,8 @@
+registerPrismBucket("pragmaticPrism", [
+    "Movement Speed",
+    "Cooldown Reduction",
+    "Lucky Hit Chance",
+    "Crowd Control Duration",
+    "Shrines Duration",
+    "Dodge Chance after Evade"
+]);
