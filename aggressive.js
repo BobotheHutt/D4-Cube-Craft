62a07ce9e3c30b57bcee8a57957a0e0dc450ebe4
BobotheHutt <jumprun4112@gmail.com>
2026-06-05 19:00:53 -0700

    Create aggressive.js

---
+++ aggressive.js
@@ -0,0 +1,14 @@
+registerPrismBucket("aggressivePrism", [
+    "Critical Strike Damage",
+    "Vulnerable Damage",
+    "Overpower Damage",
+    "All Damage %",
+    "Attack Speed",
+    "Critical Strike Chance",
+    "Physical Damage %",
+    "Fire Damage %",
+    "Lightning Damage %",
+    "Cold Damage %",
+    "Poison Damage %",
+    "Shadow Damage %"
+]);
