0838895b0676717994fd9e37661faaf10b6b3b1f
BobotheHutt <jumprun4112@gmail.com>
2026-06-05 19:01:03 -0700

    Create protector.js

---
+++ protector.js
@@ -0,0 +1,11 @@
+registerPrismBucket("protectorPrism", [
+    "Maximum Life",
+    "Total Armor",
+    "Damage Reduction %",
+    "Dodge Chance",
+    "Fortify Generation",
+    "Life on Hit",
+    "Life on Kill",
+    "Life Regeneration",
+    "All Resistance %"
+]);
