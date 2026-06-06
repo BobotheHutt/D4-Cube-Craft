45bd36a1c18ecad1aaa1043e80efba2121dd66a7
BobotheHutt <jumprun4112@gmail.com>
2026-06-05 19:01:26 -0700

    Create resourceful.js

---
+++ resourceful.js
@@ -0,0 +1,10 @@
+registerPrismBucket("resourcefulPrism", [
+    "Maximum Mana",
+    "Maximum Fury",
+    "Maximum Energy",
+    "Maximum Spirit",
+    "Maximum Essence",
+    "Resource Cost Reduction %",
+    "Resource Generation %",
+    "Resource on Kill"
+]);
